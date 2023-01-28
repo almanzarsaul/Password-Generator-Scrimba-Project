f7535f52f56cf926f05a3fe86cf83e0b9455522e
Saul Almanzar <almanzarsaul@icloud.com>
2023-01-28 14:33:15 -0400

    Set text content for password fields to generated password

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+let passwordOneEl = document.getElementById("password-field-one");
+let passwordTwoEl = document.getElementById("password-field-two");
 
 function generatePassword() {
     let password = ""
@@ -10,6 +12,7 @@ function generatePassword() {
 }
 
 function getPasswords() {
-     console.log(generatePassword());
+    passwordOneEl.textContent = generatePassword();
+    passwordTwoEl.textContent = generatePassword();
 }
 
