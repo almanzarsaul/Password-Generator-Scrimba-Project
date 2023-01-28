2ada44608e741d30f9180f7f0026fa5b79d34b20
Saul Almanzar <almanzarsaul@icloud.com>
2023-01-28 16:00:17 -0400

    Implement symbols and numbers options

--- script.js
+++ script.js
@@ -91,16 +91,32 @@ const characters = [
   "?",
   "/",
 ];
+let charactersToUse = characters.slice(0, 52); // User always uses letters at least.
 let passwordOneEl = document.getElementById("password-field-one");
 let passwordTwoEl = document.getElementById("password-field-two");
 let sliderEl = document.getElementById("length-slider");
 let lengthValueEl = document.getElementById("length-value");
 let defaultValue = 16;
 
+let useSymbols = false;
+let useNumbers = true;
+
 window.addEventListener("load", function () {
   sliderEl.value = defaultValue;
   adjustLength();
 });
+
+// 91 total characters
+// 52 letters [0-51]
+// 10 numbers [52-62]
+// 30 symbols [63-92]
+if (useSymbols === true && useNumbers === false) {
+  charactersToUse = charactersToUse.concat(characters.slice(63, 92)); // if symbols is selected but not numbers, append symbols.
+} else if (useSymbols === false && useNumbers === true) {
+  charactersToUse = charactersToUse.concat(characters.slice(52, 62)); // if symbols is not selected but numbers is, append numbers.
+} else {
+  charactersToUse = characters; // if both are selected, just use all characters
+}
 
 function adjustLength() {
   lengthValueEl.textContent = sliderEl.value;
@@ -109,8 +125,8 @@ function adjustLength() {
 function generatePassword(lengthOfPassword) {
   let password = "";
   for (let i = 0; i < lengthOfPassword; i++) {
-    let randomIndex = Math.floor(Math.random() * characters.length); // Gets a random index based on the length of the characters array
-    password += characters[randomIndex];
+    let randomIndex = Math.floor(Math.random() * charactersToUse.length); // Gets a random index based on the length of the characters array
+    password += charactersToUse[randomIndex];
   }
   return password;
 }
@@ -122,6 +138,7 @@ function getPasswords() {
 
 function copyToClipboard(text) {
   if (text !== "") {
+    // Fields are empty by default
     // Copy the text inside the text field
     navigator.clipboard.writeText(text);
 
